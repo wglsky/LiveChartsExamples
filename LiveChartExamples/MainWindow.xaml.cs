36bb9857723be17decaeccf92ce25d89f3a7aa01
guow <guow@kt.dtu.dk>
2021-08-17 22:58:15 +0200

    added nameSpace in CS file

--- LiveChartExamples/MainWindow.xaml.cs
+++ LiveChartExamples/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
+
 
 namespace LiveChartExamples
 {
